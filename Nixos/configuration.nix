992ca18bee696ad46ebd0f2dbafc4095a173620e
Cikuozzo <roccorotondo@tutamail.com>
2026-09-11 23:24:13 +0200

    added system support for AppImage

--- Nixos/configuration.nix
+++ Nixos/configuration.nix
@@ -64,6 +64,10 @@
     ];
   };
 
+  # AppImage
+  programs.appimage.enable = true;
+  programs.appimage.binfmt = true;
+
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   # environment.systemPackages = with pkgs; [
