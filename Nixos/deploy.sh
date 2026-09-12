e1ee81ff95a8be1a4f9f0f9e316a7f8a9a381523
Cikuozzo <roccorotondo@tutamail.com>
2026-09-12 04:45:23 +0200

    Updated deploy script

--- Nixos/deploy.sh
+++ Nixos/deploy.sh
@@ -4,21 +4,21 @@ set -e # Interrompe lo script in caso di errore
 
 NIXOS_DOTFILES="/home/rocco/Documenti/Github/dotfiles/Nixos"
 
-echo "==> Deploy NixOS e Home Manager..."
+echo "==> Deploying NixOS e Home Manager..."
 
 # 1. Rimuove configuration.nix e lo sostituisce con il symlink
-echo "--> Aggiornamento configuration.nix..."
+echo "--> Updating configuration.nix..."
 sudo rm -f /etc/nixos/configuration.nix
 sudo ln -s "$NIXOS_DOTFILES/configuration.nix" /etc/nixos/configuration.nix
 
 # 2. Symlink della cartella modules in /etc/nixos/
-echo "--> Collegamento cartella modules..."
+echo "--> Creating modules symlink..."
 sudo ln -sfn "$NIXOS_DOTFILES/modules" /etc/nixos/modules
 
 # 3. Symlink della cartella home-manager in ~/.config/
-echo "--> Collegamento cartella home-manager..."
+echo "--> Creating home-manager symlink..."
 ln -sfn "$NIXOS_DOTFILES/home-manager" "$HOME/.config/home-manager"
 
-echo "✓ Symlink creati con successo."
+echo "✓ Symlink created."
 
-echo "==> Deploy completato!"
+echo "==> Deploy done!"
